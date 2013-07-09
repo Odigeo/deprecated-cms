d93f586ea551e36074398dc86a89e851e6701bf4
Peter Bengtson <peter@peterbengtson.com>
2013-07-10 01:38:45 +0200

    Corrected BAN version number generation.

--- app/observers/text_observer.rb
+++ app/observers/text_observer.rb
@@ -3,18 +3,18 @@ class TextObserver < ActiveRecord::Observer
   def after_create(model)
     v = model.class.latest_api_version
   	# puts "Created #{model.inspect}"
-    Api.ban "/#{v}/texts"
-    Api.ban "/#{v}/dictionaries/app/#{model.app}/locale/#{model.locale}"
+    Api.ban "/v#{v}/texts"
+    Api.ban "/v#{v}/dictionaries/app/#{model.app}/locale/#{model.locale}"
   end
 
 
   def after_update(model)
     v = model.class.latest_api_version
     # puts "Updated #{model.inspect}"
-    Api.ban "/#{v}/texts/#{model.id}"
-    Api.ban "/#{v}/texts/#{model.id}/", true
-    Api.ban "/#{v}/texts"
-    Api.ban "/#{v}/dictionaries/app/#{model.app}/locale/#{model.locale}"
+    Api.ban "/v#{v}/texts/#{model.id}"
+    Api.ban "/v#{v}/texts/#{model.id}/", true
+    Api.ban "/v#{v}/texts"
+    Api.ban "/v#{v}/dictionaries/app/#{model.app}/locale/#{model.locale}"
   end
 
 
@@ -26,10 +26,10 @@ class TextObserver < ActiveRecord::Observer
   def after_destroy(model)
     v = model.class.latest_api_version
     # puts "Destroyed #{model.inspect}"
-    Api.ban "/#{v}/texts/#{model.id}"
-    Api.ban "/#{v}/texts/#{model.id}/", true
-    Api.ban "/#{v}/texts"
-    Api.ban "/#{v}/dictionaries/app/#{model.app}/locale/#{model.locale}"
+    Api.ban "/v#{v}/texts/#{model.id}"
+    Api.ban "/v#{v}/texts/#{model.id}/", true
+    Api.ban "/v#{v}/texts"
+    Api.ban "/v#{v}/dictionaries/app/#{model.app}/locale/#{model.locale}"
   end
 
 end
