cac93d376d0aca6c2fb8f5fd3eebefa0f50a92b9
Peter Bengtson <peter@peterbengtson.com>
2013-07-12 18:36:38 +0200

    Text observer reduced, as the automatic invalidation does the lion share.

--- app/observers/text_observer.rb
+++ app/observers/text_observer.rb
@@ -2,16 +2,16 @@ class TextObserver < ActiveRecord::Observer
  
   def after_create(model)
     v = model.class.latest_api_version
-    Api.ban "/#{v}/texts"
+    #Api.ban "/#{v}/texts"                       # - Already handled by the ocean gem
     Api.ban "/#{v}/dictionaries/app/#{model.app}/locale/#{model.locale}"
   end
 
 
   def after_update(model)
     v = model.class.latest_api_version
-    Api.ban "/#{v}/texts/#{model.id}"
-    Api.ban "/#{v}/texts/#{model.id}/", true
-    Api.ban "/#{v}/texts"
+    #Api.ban "/#{v}/texts/#{model.id}"           # - Already handled by the ocean gem
+    #Api.ban "/#{v}/texts/#{model.id}/", true    # - Already handled by the ocean gem
+    #Api.ban "/#{v}/texts"                       # - Already handled by the ocean gem
     Api.ban "/#{v}/dictionaries/app/#{model.app}/locale/#{model.locale}"
   end
 
