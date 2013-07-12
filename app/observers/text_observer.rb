d3a6e699b2ce82567ba907db244d300c3f88c423
Peter Bengtson <peter.bengtson@odigeo.com>
2013-07-12 14:57:11 +0200

    DRYed up the BasicBanObserver.

--- app/observers/text_observer.rb
+++ app/observers/text_observer.rb
@@ -2,7 +2,6 @@ class TextObserver < ActiveRecord::Observer
  
   def after_create(model)
     v = model.class.latest_api_version
-  	# puts "Created #{model.inspect}"
     Api.ban "/#{v}/texts"
     Api.ban "/#{v}/dictionaries/app/#{model.app}/locale/#{model.locale}"
   end
@@ -10,7 +9,6 @@ class TextObserver < ActiveRecord::Observer
 
   def after_update(model)
     v = model.class.latest_api_version
-    # puts "Updated #{model.inspect}"
     Api.ban "/#{v}/texts/#{model.id}"
     Api.ban "/#{v}/texts/#{model.id}/", true
     Api.ban "/#{v}/texts"
@@ -24,12 +22,7 @@ class TextObserver < ActiveRecord::Observer
 
 
   def after_destroy(model)
-    v = model.class.latest_api_version
-    # puts "Destroyed #{model.inspect}"
-    Api.ban "/#{v}/texts/#{model.id}"
-    Api.ban "/#{v}/texts/#{model.id}/", true
-    Api.ban "/#{v}/texts"
-    Api.ban "/#{v}/dictionaries/app/#{model.app}/locale/#{model.locale}"
+    after_update(model)
   end
 
 end
