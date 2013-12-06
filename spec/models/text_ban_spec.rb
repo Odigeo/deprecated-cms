29fe815949fc8484a96c27019502341e9d1531dc
Peter Bengtson <peter@peterbengtson.com>
2013-12-06 14:03:21 +0100

    Fixed specs with unescaped BAN regexes.

--- spec/models/text_ban_spec.rb
+++ spec/models/text_ban_spec.rb
@@ -8,8 +8,8 @@ describe Text do
 
 
   it "should trigger two BANs when created" do
-    Api.should_receive(:call_p).once.with("http://127.0.0.1", :ban, "/v[0-9]+/texts#{INVALIDATE_COLLECTION_DEFAULT.first}")
-    Api.should_receive(:call_p).once.with("http://127.0.0.1", :ban, "/v[0-9]+/dictionaries/app/foo_app/locale/sv-SE($|\\?)")
+    Api.should_receive(:call_p).once.with("http://127.0.0.1", :ban, Api.escape("/v[0-9]+/texts#{INVALIDATE_COLLECTION_DEFAULT.first}"))
+    Api.should_receive(:call_p).once.with("http://127.0.0.1", :ban, Api.escape("/v[0-9]+/dictionaries/app/foo_app/locale/sv-SE($|\\?)"))
   	create :text, app: "foo_app", locale: "sv-SE"
   end
 
@@ -18,9 +18,9 @@ describe Text do
     Api.stub(:call_p)
    	m = create :text, app: "foo_app", locale: "sv-SE"
   	m.result = "Zalagadoola"
-    Api.should_receive(:call_p).once.with("http://127.0.0.1", :ban, "/v[0-9]+/texts/#{m.id}#{INVALIDATE_MEMBER_DEFAULT.first}")
-    Api.should_receive(:call_p).once.with("http://127.0.0.1", :ban, "/v[0-9]+/texts#{INVALIDATE_COLLECTION_DEFAULT.first}")
-    Api.should_receive(:call_p).once.with("http://127.0.0.1", :ban, "/v[0-9]+/dictionaries/app/foo_app/locale/sv-SE($|\\?)")
+    Api.should_receive(:call_p).once.with("http://127.0.0.1", :ban, Api.escape("/v[0-9]+/texts/#{m.id}#{INVALIDATE_MEMBER_DEFAULT.first}"))
+    Api.should_receive(:call_p).once.with("http://127.0.0.1", :ban, Api.escape("/v[0-9]+/texts#{INVALIDATE_COLLECTION_DEFAULT.first}"))
+    Api.should_receive(:call_p).once.with("http://127.0.0.1", :ban, Api.escape("/v[0-9]+/dictionaries/app/foo_app/locale/sv-SE($|\\?)"))
  	  m.save!
   end
 
@@ -28,9 +28,9 @@ describe Text do
   it "should trigger three BANs when touched" do
     Api.stub(:call_p)
   	m = create :text, app: "foo_app", locale: "sv-SE"
-    Api.should_receive(:call_p).once.with("http://127.0.0.1", :ban, "/v[0-9]+/texts/#{m.id}#{INVALIDATE_MEMBER_DEFAULT.first}")
-    Api.should_receive(:call_p).once.with("http://127.0.0.1", :ban, "/v[0-9]+/texts#{INVALIDATE_COLLECTION_DEFAULT.first}")
-    Api.should_receive(:call_p).once.with("http://127.0.0.1", :ban, "/v[0-9]+/dictionaries/app/foo_app/locale/sv-SE($|\\?)")
+    Api.should_receive(:call_p).once.with("http://127.0.0.1", :ban, Api.escape("/v[0-9]+/texts/#{m.id}#{INVALIDATE_MEMBER_DEFAULT.first}"))
+    Api.should_receive(:call_p).once.with("http://127.0.0.1", :ban, Api.escape("/v[0-9]+/texts#{INVALIDATE_COLLECTION_DEFAULT.first}"))
+    Api.should_receive(:call_p).once.with("http://127.0.0.1", :ban, Api.escape("/v[0-9]+/dictionaries/app/foo_app/locale/sv-SE($|\\?)"))
  	  m.touch
   end
 
@@ -38,9 +38,9 @@ describe Text do
   it "should trigger three BANs when destroyed" do
     Api.stub(:call_p)
   	m = create :text, app: "foo_app", locale: "sv-SE"
-    Api.should_receive(:call_p).once.with("http://127.0.0.1", :ban, "/v[0-9]+/texts/#{m.id}#{INVALIDATE_MEMBER_DEFAULT.first}")
-    Api.should_receive(:call_p).once.with("http://127.0.0.1", :ban, "/v[0-9]+/texts#{INVALIDATE_COLLECTION_DEFAULT.first}")
-    Api.should_receive(:call_p).once.with("http://127.0.0.1", :ban, "/v[0-9]+/dictionaries/app/foo_app/locale/sv-SE($|\\?)")
+    Api.should_receive(:call_p).once.with("http://127.0.0.1", :ban, Api.escape("/v[0-9]+/texts/#{m.id}#{INVALIDATE_MEMBER_DEFAULT.first}"))
+    Api.should_receive(:call_p).once.with("http://127.0.0.1", :ban, Api.escape("/v[0-9]+/texts#{INVALIDATE_COLLECTION_DEFAULT.first}"))
+    Api.should_receive(:call_p).once.with("http://127.0.0.1", :ban, Api.escape("/v[0-9]+/dictionaries/app/foo_app/locale/sv-SE($|\\?)"))
   	m.destroy
   end
 
